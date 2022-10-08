43baff24421d4ce49a02713ccbd9ec5c118c91f6
wooniekim <dawun3295@gmail.com>
2022-10-08 19:41:52 +0900

    "initial commit"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import Nav from './components/nav';
 // import Footer from './components/footer';
-// import Router from './router';
-// import Register from "./pages/registerPage/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Register from "./pages/registerPage/Register";
 import Login from "./pages/loginPage/Login";
 
 function App() {
   return (
-    <Login />
+    <Router>
+        <Route path="/auth">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+    </Router>
   );
 }
 
